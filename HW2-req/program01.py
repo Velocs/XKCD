81eb019834e0720364bfea8fe5dc36a2ddaecb80
Velocs <63161372+Velocs@users.noreply.github.com>
2022-11-04 19:09:44 +0100

    Add files via upload

---
+++ HW2-req/program01.py
@@ -0,0 +1,155 @@
+
+# -*- coding: utf-8 -*-
+"""
+Consideriamo la codifica dei numeri romani e la modifica suggerita da 
+Randall Munroe nel suo blog XKCD.
+Nella codifica dei numeri romani:
+- non esiste lo zero
+- si usano le lettere 'IVXLCDM' che corrispondono ai valori decimali
+  'I' = 1, 'V' = 5, 'X' = 10, 'L' = 50, 'C' = 100, 'D' = 500, 'M' = 1000
+- i numeri si scrivono da sinistra a destra cominciando con i valori più alti 
+  (migliaia, centinaia, decine, unità)
+- i valori delle lettere si sommano, tranne quando sono seguiti da lettere di peso maggiore, 
+  nel qual caso si sottraggono
+- si possono usare al massimo 3 lettere consecutive uguali per le lettere 'IXCM'
+  ('III' = 3, 'XXX' = 30, 'CCC' = 300 , 'MMM' = 3000)
+- per rappresentare i valori che hanno cifra decimale 4 o 9 si usa la sottrazione 
+  dalla lettera seguente
+  Es. 4 = 'IV'   9 = 'IX',    40 = 'XL'    39 = 'IXL'   499 = 'ID'
+
+Nel suo blog XKCD, invece, Randall Munroe codifica i numeri romani con i corrispondenti numeri arabi: 
+si concatenano i numeri arabi ottenuti sostituendo a ciascuna lettera il valore corrispondente.  
+Es.    397 =>  'CCCXCVII' => 100 100 100 10 100 5 1 1 => '10010010010100511'
+Chiamiamo questa codifica "formato XKCD".
+
+Obiettivo dello homework è decodificare una lista di stringhe che rappresentano
+numeri romani nel formato XKCD, e tornare i K valori maggiori in ordine decrescente.
+
+Implementate quindi le seguenti funzioni:
+"""
+
+def decode_XKCD_tuple(xkcd_values : tuple[str, ...], k : int) -> list[int]:
+    '''
+    Riceve una lista di stringhe che rappresentano numeri nel formato XKCD
+    ed un intero k positivo.
+    Decodifica i numeri forniti e ne ritorna i k maggiori.
+
+    Parameters
+    valori_xkcd : list[str]     lista di stringhe in formato XKCD
+    k : int                     numero di valori da tornare
+    Returns
+    list[int]                   i k massimi valori ottenuti in ordine decrescente
+    '''
+    i = 0
+    numeri = []
+    while (i < len(xkcd_values)):
+      numeri.append(decode_value(xkcd_values[i]))
+      i+=1
+    numeri.sort()
+    numeri.reverse()
+    return numeri
+    # INSERISCI QUI IL TUO CODICE
+    pass
+
+
+def decode_value(xkcd : str ) -> int:
+    '''
+    Decodifica un valore nel formato XKCD e torna l'intero corrispondente.
+
+    Parameters
+    xkcd : str                  stringa nel formato XKCD
+    Returns
+    int                         intero corrispondente
+    
+    Esempio: '10010010010100511' -> 397
+    '''
+    return list_of_weights_to_number(xkcd_to_list_of_weights(xkcd))
+    # INSERISCI QUI IL TUO CODICE
+    pass
+
+
+def xkcd_to_list_of_weights(xkcd : str) -> list[int]:
+    '''
+    Spezza una stringa in codifica XKCD nella corrispondente
+    lista di interi, ciascuno corrispondente al peso di una lettera romana
+
+    Parameters
+    xkcd : str              stringa nel formato XKCD
+    Returns
+    list[int]               lista di 'pesi' corrispondenti alle lettere romane
+
+    Esempio: '10010010010100511' -> [100, 100, 100, 10, 100, 5, 1, 1,]
+    '''
+    result = []
+    numeri = ["1000", "500", "100", "50", "10", "5", "1"]
+    c = 0
+    while (c <= len(xkcd) - 1):
+      for numero in numeri:
+        if (numero == xkcd[c:len(numero)+c]):
+          result.append(int(numero))
+          c += len(numero)
+          break
+    # INSERISCI QUI IL TUO CODICE
+    return result
+
+
+def list_of_weights_to_number(weigths : list[int] ) -> int:
+    '''
+    Trasforma una lista di 'pesi' nel corrispondente valore arabo
+    tenendo conto della regola di sottrazione
+
+    Parameters
+    lista_valori : list[int]    lista di 'pesi' di lettere romane
+    Returns
+    int                         numero arabo risultante
+    
+    Esempio: [100, 100, 100, 10, 100, 5, 1, 1,] -> 397
+    [1, 10, 100, 1000]
+    '''
+    i = 1
+    k = 0
+    c = 1
+    result = 0
+    temp = 0
+    while (i < len(weigths)):
+      if not (weigths[i -1] < weigths[i]):
+        result = result + weigths[i-1]
+      elif(weigths[i -1] < weigths[i]):
+        c = i
+        while (weigths[i-1] < weigths[i] and i < len(weigths) - 1):
+          i += 1
+        temp = weigths[i-1]
+        k = i -1
+        while (k >= c):
+          result = result + (temp - weigths[k - 1])
+          print(result)
+          k-=1
+      i += 1
+    result += weigths[i-1]
+    return result
+
+      # else:
+      #   result = result + weigths[i]
+      # i += 1
+      # if i < len(weigths) - 1 and weigths[i] < weigths[i+1]:
+      #   result = result + (weigths[i+1] - weigths[i])
+      #   i += 1
+      # else:
+      #   result = result + weigths[i]
+      # i += 1
+    return result
+
+    # INSERISCI QUI IL TUO CODICE
+    pass
+
+
+
+###################################################################################
+if __name__ == '__main__':
+    # inserisci qui i tuoi test
+    xkcd = ["150",  "1050110" , "100100010100110", "11000", "1500", "10050010100110"]
+    # print('10010010010100511', decode_value('10010010010100511'), '(397?)')
+    # print(xkcd_to_list_of_weights("10010010010100511"))
+    # xkcd_to_list_of_weights("10010010010100511")
+    # print(decode_XKCD_tuple(xkcd, 1))
+    print(decode_value("1101001000"))
